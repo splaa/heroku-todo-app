9327f505c5597830907831f6076d7204b771e949
splx <splaandrey@gmail.com>
2022-08-13 16:26:00 +0300

    added rocket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "hello, world!"
 }
+
+#[rocket::main]
+async fn main() {
+    let _ =  rocket::build()
+        .mount("/", routes![index])
+        .launch()
+        .await
+        .expect("panic message");
+}
+
